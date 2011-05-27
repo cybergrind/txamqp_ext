85de6132086e3c28de6e976b4d72967be5868205
cbr work <cybergrind@gmail.com>
2011-05-27 16:01:11 +0300

    add message sending loop

--- txamqp_ext/protocol.py
+++ txamqp_ext/protocol.py
@@ -1,12 +1,15 @@
 
 import logging
+import time
 
 from zope.interface import implements
+from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 from twisted.internet.defer import DeferredList
 
 from txamqp.client import TwistedDelegate
 from txamqp.protocol import AMQClient
+from txamqp.content import Content
 import txamqp.spec
 
 from txamqp_ext.interfaces import IAmqpProtocol
@@ -47,7 +50,7 @@ class AmqpProtocol(AMQClient):
         d = self.authenticate(self.factory.user, self.factory.password)
         d.addCallback(self._authenticated)
         d.addErrback(self._error)
-        d.addCallback(self._auth_succ.callback)
+        #d.addCallback(self._auth_succ.callback)
         return d
 
     def _authenticated(self, _none):
@@ -59,7 +62,7 @@ class AmqpProtocol(AMQClient):
         wd = self.channel(2)
         wd.addCallback(self._open_write_channel)
         wd.addErrback(self._error)
-        
+        self._auth_succ.callback(True)
         dl = DeferredList([rd, wd])
         return dl
 
@@ -76,15 +79,62 @@ class AmqpProtocol(AMQClient):
         self.write_chan = chan
         def _opened(*args):
             self._write_opened.callback(self.write_chan)
+            self.send_loop()
         d = self.write_chan.channel_open()
         d.addCallback(_opened)
         d.addErrback(self._error)
         return d
 
     def _error(self, failure):
-        print dir(failure)
         print failure.getTraceback()
         raise failure
+
+    def send_loop(self, *args):
+        # check for messages waiting sending
+        if self.factory.processing_send:
+            msg = self.factory.processing_send
+            self.process_message(msg)
+        else:
+            msg = self.factory.send_queue.get()
+            msg.addCallback(self.process_message)
+            msg.addErrback(self._error)
+
+    def process_message(self, msg):
+        # run one more task, if we have parallel factory
+        if self.factory.parallel:
+            reactor.callLater(0, self.send_loop)
+
+        self.factory.processing_send = msg
+        exc = msg.get('exchange')
+        rk = msg.get('rk')
+        content = msg.get('content')
+        cb = msg.get('callback')
+        # convert to content type if not Content
+        if type(content) != Content:
+            content = Content(content)
+        # setup tid in Content.properties
+        # tid will be unique id for back message
+        if msg.get('tid'):
+            content['tid'] = msg['tid']
+        else:
+            content['tid'] = int(time.time()*1e7)
+        # set delivery mode if not provided
+        if not content.properties.get('delivery mode'):
+            content['delivery mode'] = getattr(self.factory, 'delivery_mode', 2)
+        def _after_send(res):
+            if not cb.called:
+                cb.callback(res)
+            self.factory.processing_send = None
+            # if we have non-parallel factory
+            # we should run next message only after
+            # previous has been processed
+            if not self.factory.parallel:
+                reactor.callLater(0, self.send_loop)
+        w = self.write_chan.basic_publish(exchange=exc,
+                                          routing_key=rk,
+                                          content=content)
+        w.addCallback(_after_send)
+        return w
 
     def on_read_channel_opened(self):
         return self._read_opened
--- txamqp_ext/test/test_protocol.py
+++ txamqp_ext/test/test_protocol.py
@@ -1,15 +1,21 @@
+import time
 
 from twisted.trial.unittest import TestCase
 from twisted.internet import reactor
 from twisted.internet import protocol
 from twisted.internet.defer import Deferred
 from twisted.internet.defer import DeferredList
+from twisted.internet.defer import DeferredQueue
 
 from txamqp.client import TwistedDelegate
 import txamqp.spec
 
 from txamqp_ext.protocol import AmqpProtocol
 
+
+EXC = 'test_exchange'
+QUE = 'test_queue'
+RK = 'test_route'
 
 class TestFactory(protocol.ClientFactory):
     protocol = AmqpProtocol
@@ -24,6 +30,9 @@ class TestFactory(protocol.ClientFactory):
         self.connected = Deferred()
         self.connected.addErrback(self._err)
         self.err_fail = True
+        self.send_queue = DeferredQueue()
+        self.processing_send = None
+        self.parallel = True
         reactor.connectTCP(self.host, self.port, self)
 
     def startedConnecting(self, connector):
@@ -56,16 +65,23 @@ class TestFactory(protocol.ClientFactory):
         return self.client.transport.loseConnection()
 
 
-class ProtocolTest(TestCase):
+class ProtocolA(TestCase):
+    '''
+    Tests for connection
+    '''
     def setUp(self):
         self.f = TestFactory()
         self.failError = True
 
     def test_001_connect(self):
-        def _conn(*args):
-            d = self.f.client._auth_succ
+        def _authed(*args):
+            pass
+        def _conn(c):
+            d = c._auth_succ
+            d.addCallback(_authed).addErrback(self.f._err)
             return d
-        return self.f.connected.addCallback(_conn)
+        d = self.f.connected.addCallback(_conn).addErrback(self.f._err)
+        return d
 
     def test_002_read_opened(self):
         def _on_connect(*args):
@@ -87,4 +103,80 @@ class ProtocolTest(TestCase):
         self.f.err_fail = False
         return self.f.close_transport()
         
-        
+class ProtocolB(TestCase):
+    '''
+    Connected tests
+    '''
+    def setUp(self):
+        self.f = TestFactory()
+        self.failError = True
+        return self.f.connected
+
+    def test_001_test_define(self):
+        def _defined_resp(resp):
+            assert resp.method.name=='declare-ok', resp
+        def _on_write_open(w_chan):
+            d1 = w_chan.exchange_declare(exchange=EXC,
+                                        type='topic',
+                                        durable=True,
+                                        auto_delete=False)
+            d1.addCallback(_defined_resp)
+
+            d2 = w_chan.queue_declare(queue=QUE,
+                                      durable=True,
+                                      auto_delete=False)
+            d2.addCallback(_defined_resp)
+            return DeferredList([d1, d2])
+        d = self.f.client.on_write_channel_opened()
+        d.addCallback(_on_write_open)
+        return d
+
+    def test_002_test_par_send(self):
+        self.f.parallel = True
+        NUM = 1000
+        self.s = 0
+        t = time.time()
+        def _sended(res):
+            self.s += 1
+        def _test_num_sended(res):
+            assert self.s == NUM, 'sended: %r'%self.s
+            print 'TIME: %.4f NUM: %r'%(time.time()-t, NUM)
+        def _send_def():
+            return Deferred().addCallback(_sended).addErrback(self.f._err)
+        dl = []
+        for i in xrange(NUM):
+            d = _send_def()
+            msg = {'exchange': EXC,
+                   'rk': RK,
+                   'content': 'cnt',
+                   'callback': d}
+            self.f.send_queue.put(msg)
+            dl.append(d)
+        return DeferredList(dl).addCallback(_test_num_sended)
+
+    def test_003_test_not_par_send(self):
+        self.f.parallel = False
+        NUM = 1000
+        self.s = 0
+        t = time.time()
+        def _sended(res):
+            self.s += 1
+        def _test_num_sended(res):
+            assert self.s == NUM, 'sended: %r'%self.s
+            print 'TIME: %.4f NUM: %r'%(time.time()-t, NUM)
+        def _send_def():
+            return Deferred().addCallback(_sended).addErrback(self.f._err)
+        dl = []
+        for i in xrange(NUM):
+            d = _send_def()
+            msg = {'exchange': EXC,
+                   'rk': RK,
+                   'content': 'cnt',
+                   'callback': d}
+            self.f.send_queue.put(msg)
+            dl.append(d)
+        return DeferredList(dl).addCallback(_test_num_sended)
+
+    def tearDown(self):
+        self.f.err_fail = False
+        return self.f.close_transport()
